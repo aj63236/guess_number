73ff63538f236c6ab3de9617343164057156930f
aj63236 <59002970+aj63236@users.noreply.github.com>
2021-12-05 10:36:40 +0800

    Ver 2.0

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,10 @@
 import random
 r = random.randint(1 , 100)
 print(r)
+i = 1
 
 while True:
+    print("這是第%s次猜測" %i)
     guess = input("請猜一個數字:")
     guess = int(guess)
     if guess == r:
@@ -12,3 +14,4 @@ while True:
         print("要猜得比答案小")
     else:
         print("要猜得比答案大")
+    i = i + 1
